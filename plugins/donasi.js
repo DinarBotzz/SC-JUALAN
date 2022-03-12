beb0a61501057464b025312c36751ac246c6159d
DinarBotzz <101435875+DinarBotzz@users.noreply.github.com>
2022-03-12 23:10:27 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 089504575881
-├ DANA : 089504575881 
-├ OVO : 089504575881
-├ Shoppe : 089504575881
-├ GOPAY : 089504575881 
+├ PULSA : 085774408271
+├ DANA : 085774408271
+├ OVO : 085774408271
+├ Shoppe : 085774408271
+├ GOPAY : 085774408271
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
