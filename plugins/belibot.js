bbbd16f4cc42101bb082390a427889a8abb8efdb
DinarBotzz <101435875+DinarBotzz@users.noreply.github.com>
2022-03-13 06:09:15 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,19 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 15000*
-╠➥ *Permanen* : *Rp 25000*
-╠➥ *Premium* :   *Rp 10000*
-╠➥ *Sc Bot* :        *Beli*
+╠➥ *1 Minggu* :    *RP 5.000*
+╠➥ *1 Bulan* :      *Rp 30.000*
+╠➥ *Permanen* : *Rp 50.000*
+╠➥ *Premium* :   *Rp 20.000*
+║
 ║
 ╠═〘 PEMBAYARAN 〙 ═
-╠➥ Gopay, Dana, Dan Pulsa
+╠➥ Gopay, Dana, Dan Ovo
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
-╠═ ©2021 
-╠═ Scrip original by Tio
+╠═ ©2022
+╠═ Scrip original by Dinar Botz
 ╠═〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Gopay', '#viagopay', m)
 }
 
